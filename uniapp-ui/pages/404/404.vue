fe1afdbcde8403a804bce5f850e7dccb3da0176c
LOUSANPANG <1271255653@qq.com>
2021-06-16 17:39:59 +0800

    feat: :bento: add 404

---
+++ uniapp-ui/pages/404/404.vue
@@ -0,0 +1,20 @@
+<template>
+	<view class="nofound">
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'Nofound',
+
+		data() {
+			return {}
+		},
+
+		methods: {}
+
+	}
+</script>
+
+<style lang="scss">
+</style>
