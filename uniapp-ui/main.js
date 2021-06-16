6c0e208c1643b38cf1a14eb41f6422179f8bc410
LOUSANPANG <1271255653@qq.com>
2021-06-16 17:37:54 +0800

    feat: :white_check_mark: 新增路由拦截功能

--- uniapp-ui/main.js
+++ uniapp-ui/main.js
@@ -1,17 +1,23 @@
 import Vue from 'vue'
 import App from './App'
+import { router, RouterMount } from './router.js'
 // import store from '@/store'
 import CONFIG from './config.js'
 
-
 Vue.prototype.$CONFIG = CONFIG
-
 Vue.config.productionTip = false
-
+Vue.use(router)
 App.mpType = 'app'
 
 const app = new Vue({
 	// store,
 	...App
 })
-app.$mount()
+
+// #ifdef H5
+	RouterMount(app,router, '#app')
+// #endif
+
+// #ifndef H5
+	app.$mount()
+// #endif
---
+++ uniapp-ui/router.js
@@ -0,0 +1,40 @@
+// router
+import { RouterMount, createRouter } from 'uni-simple-router'
+import { GetStorageSync } from '@/utils/custom_storage.js'
+
+const router = createRouter({
+	platform: process.env.VUE_APP_PLATFORM,
+	routes: [
+		...ROUTES,
+		{
+			path: '*',
+			redirect: (to) => {
+				return { name: '404' }
+			}
+		}
+	]
+})
+
+//全局路由前置守卫
+router.beforeEach((to, from, next) => {
+	const loginAuth = to.meta.loginAuth
+	const token = GetStorageSync('token')
+	if (loginAuth) {
+		token
+			? next()
+			: next({
+					name: 'login',
+					params: { formPath: to.fullPath }
+				})
+	} else {
+		next()
+	}
+})
+
+// 全局路由后置守卫
+router.afterEach((to, from) => {})
+
+export {
+	router,
+	RouterMount
+}
