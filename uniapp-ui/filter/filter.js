b6c5ab4ccac6688e9a46548619a3e4ff5073a0d3
LOUSANPANG <1271255653@qq.com>
2021-11-15 15:50:06 +0800

    feat: 添加卡片格式化filter

--- uniapp-ui/filter/filter.js
+++ uniapp-ui/filter/filter.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
 
 // 999999 => ¥999,999.00
-Vue.filter('appointFormatMoney', function(num, options = { style: 'currency', currency: 'CNY' }) {
+Vue.filter('filerFormatMoney', function(num, options = { style: 'currency', currency: 'CNY' }) {
 	if (!num) return ''
 	return (Number(num)).toLocaleString('zh-CN', options)
 })
 
 // 153****0730
-Vue.filter('appointFormatPhone', function(phone) {
+Vue.filter('filerFormatPhone', function(phone) {
 	if (!phone) return ''
 	let newPhone = phone.replace(/(\d{3})\d{4}(\d{4})/, '$1****$2')
 	return newPhone
+})
+
+// 1234 5678 9012 3456 789
+Vue.filter('filerFormatCardNo', function(cardNo) {
+	return String(cardNo).replace(/(\d{4})/g, "$1 ").trim()
 })
