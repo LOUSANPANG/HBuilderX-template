ea4a0defcf9c09fa445473fb157b02a435a1f76a
LOUSANPANG <1271255653@qq.com>
2022-04-07 08:57:08 +0800

    feat:  添加过滤器手机号格式化、反显

--- uniapp-ui/filter/filter.js
+++ uniapp-ui/filter/filter.js
@@ -7,9 +7,15 @@ Vue.filter('filerFormatMoney', function(num, options = { style: 'currency', curr
 })
 
 // 153****0730
+Vue.filter('filerEncryptPhone', function(phone) {
+	if (!phone) return ''
+	let newPhone = String(phone).replace(/(\d{3})\d{4}(\d{4})/, '$1****$2')
+	return newPhone
+})
+// 153 xxxx xxxx
 Vue.filter('filerFormatPhone', function(phone) {
 	if (!phone) return ''
-	let newPhone = phone.replace(/(\d{3})\d{4}(\d{4})/, '$1****$2')
+	let newPhone = String(phone).replace(/\s/g, '').replace(/(\d{3})(\d{0,4})(\d{0,4})/, '$1 $2 $3')
 	return newPhone
 })
 
